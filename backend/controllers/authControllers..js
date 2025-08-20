3782a500751b0b11a69a45914295837e3b4cdfe0
Roshan Patel <roshanpatel12309@gmail.com>
2025-08-20 23:28:20 +0530

    Manage controllers

---
+++ backend/controllers/authControllers..js
@@ -0,0 +1,9 @@
+export const signup = (req, res) => {
+    res.send('Yeah this is signup platform')
+}
+export const login = (req, res) => {
+    res.send('Yeah this is Login platform')
+}
+export const logout = (req, res) => {
+    res.send('Yeah this is Logout platform')
+}
