b7d9c0df9f1bc87794725c650bf16a3e2238931e
Roshan Patel <roshanpatel12309@gmail.com>
2025-08-22 22:17:53 +0530

    Created user model

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -15,3 +15,7 @@ const userSchema = new mongoose.Schema({
         required:true
     }
 })
+
+const userModel = mongoose.models.user || mongoose.model('user',userSchema);
+
+export default userModel;
