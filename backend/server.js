51261004b60bd4fe12d49d9f711b6687d420b112
Roshan Patel <roshanpatel12309@gmail.com>
2025-08-22 22:19:09 +0530

    Debug and fix

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,18 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import authRouter from './routes/authRoutes.js';
+import connectDB from './config/mongodb.js';
+import cookieParser from 'cookie-parser';
+
 
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 4000;
+
+app.use(express.json());
+app.use(cookieParser());
+
+connectDB();
 
 app.get('/',(req,res) => {
     res.send("API working")
