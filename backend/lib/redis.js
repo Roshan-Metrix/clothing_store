34b51cca0cc324f47df6ffa14f0ab7e3f95d7c1e
Roshan Patel <roshanpatel12309@gmail.com>
2025-09-05 09:57:30 +0530

    Configure redis setting

--- backend/lib/redis.js
+++ backend/lib/redis.js
@@ -7,7 +7,12 @@ export const redis = createClient({
   url: process.env.UPSTASH_REDIS_URL
 });
 
-redis.on("error", function(err) {
-  throw err;
-});
-await redis.connect()
+async function connectRedis() {
+  await redis.connect();
+}
+
+connectRedis();
+
+if(!connectRedis){
+  console.log("Redis Not Connected");
+}
