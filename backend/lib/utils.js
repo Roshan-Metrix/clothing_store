cf48f3f72fa3eabf95efed68d338062b92aae1d0
Roshan Patel <roshanpatel12309@gmail.com>
2025-09-05 09:57:58 +0530

    Configure tokens functions

--- backend/lib/utils.js
+++ backend/lib/utils.js
@@ -1,18 +1,36 @@
 import jwt from 'jsonwebtoken'
+import { redis } from './redis.js';
 
-export const generateToken = (userId,res) => {
+export const generateTokens = (userId,res) => {
+  const accessToken = jwt.sign({id:userId},process.env.ACCESS_TOKEN_SECRET,{
+        expiresIn: "15min",
+    })
 
-  const token = jwt.sign({id:userId},process.env
-    .JWT_SECRET,{
+  const refreshToken = jwt.sign({id:userId},process.env.REFRESH_TOKEN_SECRET,{
         expiresIn: "7d",
     })
 
-   res.cookie("token",token,{
-    httpOnly:true,
-    secure:process.env.NODE_ENV === "production",
-    sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
-    maxAge: 7 * 24 * 60 * 60 * 1000,
-   })
-   
-   return token;
+   return {accessToken, refreshToken};
 }
+
+export const storeRefreshToken = async (res, userId, refreshToken) => {
+   await redis.set(`refresh_token:${userId}`,refreshToken,"EX",7*24*60*60); // 7 days
+}
+
+export const setCookies = (res, accessToken, refreshToken) => { 
+
+  res.cookie("accessToken", accessToken, {
+    httpOnly: true, // prevent XSS attacks, cross site scripting attack
+    secure: process.env.NODE_ENV === "production",
+    sameSite: "strict", // prevent CSRF attack, cross site request forgery attack
+    maxAge: 15 * 60 * 1000, // 15 minutes
+  });
+
+  res.cookie ("refreshToken", refreshToken, {
+    httpOnly: true, // prevent XSS attacks, cross site scripting attack
+    secure: process.env.NODE_ENV === "production",
+    sameSite: "strict", // prevent CSRF attack, cross site request forgery attack
+    maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+  });
+};
+
