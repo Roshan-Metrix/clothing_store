9c9b4ddcab20d516289a609c3b9d016817fc0117
Roshan Patel <roshanpatel12309@gmail.com>
2025-09-06 13:49:16 +0530

    Modifing refreshToken

--- backend/controllers/authControllers.js
+++ backend/controllers/authControllers.js
@@ -3,7 +3,8 @@ import jwt from "jsonwebtoken";
 import { redis } from "../lib/redis.js";
 import bcrypt from "bcryptjs";
 import { generateTokens, setCookies, storeRefreshToken } from "../lib/utils.js";
-
+import dotenv from 'dotenv'
+dotenv.config({quiet: true});
  
 export const signup = async (req, res) => {
   const { name, email, password } = req.body;
@@ -94,7 +95,7 @@ export const login = async (req, res) => {
         .json({ status: false, message: "Please register first" });
     }
   } catch (error) {
-    console.log("Error in Login Auth:" + error.message);
+    console.log("Error in Login Controller:" + error.message);
     return res.status(500).send({ success: false, message: error.message });
   }
 };
@@ -125,6 +126,47 @@ const refreshToken = req.cookies.refreshToken;
   }
 };
 
+// This will refresh the access token using the refresh token
+export const refreshToken = async (req, res) => {
+  try{
+   const refreshToken = req.cookies.refreshToken;
+   
+   if(!refreshToken){
+    return res.status(401).json({message:"No refresh token provided" });
+   }
+
+   const decoded = jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET);
+
+   console.log(decoded.userid);
+
+   const storedToken = await redis.get(`refresh_token:${decoded.userId}`);
+
+   console.log(storedToken)
+
+   if(storedToken !== refreshToken){
+    return res.status(401).json({ message:"Invalid refresh token" });
+   }
+
+   const accessToken = jwt.sign({ userId: decode.userId}, process.env.ACCESS_TOKEN_SECRET,{expiresIn: "15m"})
+ 
+   res.cookie("accessToken", accessToken,{
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: "strict",
+    maxAge: 15 * 60 * 1000
+   })
+
+   res.status(201).json({ status: true, message: "Token refreshed successfully", 
+      });
+
+  }catch (error) {
+    console.log("Error in refreshToken controllers : " + error.message);
+    res.status(500).json({ success: false, message: error.message });
+  }
+}
+
 export const isAuth = async (req, res) => {
-  const { refreshToken } = req.cookies;
+
 };
+
+
