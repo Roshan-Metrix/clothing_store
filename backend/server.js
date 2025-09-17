9b002bc8f6095f79e2a3a93ba3a1dc276c31b27e
Roshan Patel <roshanpatel12309@gmail.com>
2025-09-17 18:44:28 +0530

    Changes user setting

--- backend/server.js
+++ backend/server.js
@@ -1,30 +1,43 @@
-import express from 'express'
-import dotenv from 'dotenv'
-import connectDB from './config/mongodb.js';
-import cookieParser from 'cookie-parser';
-import authRouter from './routes/authRoutes.js';
-import productRouter from './routes/productRoutes.js';
-import cartRouter from './routes/cartRoutes.js';
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import path from "path";
 
-dotenv.config({quiet: true});
+import authRoutes from "./routes/auth.route.js";
+import productRoutes from "./routes/product.route.js";
+import cartRoutes from "./routes/cart.route.js";
+import couponRoutes from "./routes/coupon.route.js";
+import paymentRoutes from "./routes/payment.route.js";
+import analyticsRoutes from "./routes/analytics.route.js";
+
+import { connectDB } from "./lib/db.js";
+
+dotenv.config();
+
 const app = express();
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 5000;
 
-app.use(express.json());
+const __dirname = path.resolve();
+
+app.use(express.json({ limit: "10mb" })); // allows you to parse the body of the request
 app.use(cookieParser());
 
-connectDB();
+app.use("/api/auth", authRoutes);
+app.use("/api/products", productRoutes);
+app.use("/api/cart", cartRoutes);
+app.use("/api/coupons", couponRoutes);
+app.use("/api/payments", paymentRoutes);
+app.use("/api/analytics", analyticsRoutes);
 
-app.get('/',(req,res) => {
-    res.send("API working")
-})
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
-app.use('/api/auth',authRouter);
-app.use('/api/products',productRouter);
-app.use('/api/cart',cartRouter);
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+    });
+}
 
-app.listen(PORT,() => {
-    console.log(`App is running at ${PORT}`)
-})
-
-// 2:10
+app.listen(PORT, () => {
+    console.log("Server is running on http://localhost:" + PORT);
+    connectDB();
+});
